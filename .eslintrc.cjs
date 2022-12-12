aa19e9f8e3570dc4c3eb84dd51ffba0bf08d3f06
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-12-13 02:00:26 +0900

    chore: eslint 설정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,9 @@
 module.exports = {
     "env": {
         "browser": true,
-        commonjs: true,
         "es2021": true
     },
-    "extends": "airbnb-base",
+    "extends": "eslint:recommended",
     "overrides": [
     ],
     "parserOptions": {
