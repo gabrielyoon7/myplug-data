6e10904e7ad57ed615fdc41e9e5d100a48a536c3
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-12-13 21:06:24 +0900

    chore: colors 적용

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 // import * as dataManager from './src/dataManager.js'
 // dataManager.run();
 
-import mongoose from "mongoose";
-import DataManager from "./src_v3/DataManager.js";
+import mongoose from 'mongoose';
+import colors from 'colors';
+import DataManager from './src_v3/DataManager.js';
 
 const run = async () => {
-    //  Mongoose Connection (여기서만 해주면 전역 설정 됨 / 1회)
-    await mongoose
-        // .connect(`mongodb+srv://gabrielyoon7:0000@gabrielyoon7.aq0fu.mongodb.net/myplug?retryWrites=true&w=majority`, {})
-        .connect(`mongodb://localhost:27017/myplug`, {})
-        .then(() => {
-            console.log('MongoDB Connected!!');
-            const dataManager = new DataManager();
-            dataManager.init();
-        })
-        .catch(err => console.log(err));
-}
+  //  Mongoose Connection (여기서만 해주면 전역 설정 됨 / 1회)
+  await mongoose
+  // .connect(`mongodb+srv://gabrielyoon7:0000@gabrielyoon7.aq0fu.mongodb.net/myplug?retryWrites=true&w=majority`, {})
+    .connect('mongodb://localhost:27017/myplug', {})
+    .then(() => {
+      console.log('MongoDB Connected!!');
+      const dataManager = new DataManager();
+      dataManager.init();
+    })
+    .catch((err) => console.log(err));
+};
 
 run();
