2e1f09e2231f2315131ddcc34b0e6dbc3c20bbc8
gabrielyoon7 <gabrielyoon7@gmail.com>
2022-12-24 07:35:17 +0900

    refactor: 불필요한 변수 재활용 방지

--- src_v3/controllers/Logger.js
+++ src_v3/controllers/Logger.js
@@ -10,7 +10,9 @@ export default class Logger {
 
   #rawData = null;
 
-  #logsForBulk = [];
+  #newBulkDocs = [];
+
+  #usingBulkDocs = [];
 
   constructor(statusManager, region, currentPage, rawData) {
     this.#statusManager = statusManager;
@@ -37,29 +39,29 @@ export default class Logger {
       // eslint-disable-next-line no-underscore-dangle
       const index = logs.findIndex((item) => (item._id === newId));
       if (index === -1) {
-        this.#logsForBulk.push(
+        this.#newBulkDocs.push(
           insertDefaultLogDoc(this.#statusManager.getTime(), this.#region, newId, raw),
         );
       }
     });
-    console.log(`[logger] ${this.#region + this.#currentPage} next logs size : ${this.#logsForBulk.length}`);
-    await StationLogs.bulkWrite(this.#logsForBulk).then((bulkWriteOpResult) => {
-      console.log(`[logger] [신규 충전소 기본 로그 추가] ${this.#region + this.#currentPage} MongoDB BULK update OK : ${this.#logsForBulk.length}`.green);
+    console.log(`[logger] ${this.#region + this.#currentPage} next logs size : ${this.#newBulkDocs.length}`);
+    await StationLogs.bulkWrite(this.#newBulkDocs).then((bulkWriteOpResult) => {
+      console.log(`[logger] [신규 충전소 기본 로그 추가] ${this.#region + this.#currentPage} MongoDB BULK update OK : ${this.#newBulkDocs.length}`.green);
     }).catch((err) => {
       console.log(`>> Logs [신규 충전소 기본 로그 추가] ${this.#region + this.#currentPage} BULK update error`);
       console.log(JSON.stringify(err));
       console.log(err);
     });
-    this.#logsForBulk = [];
+    // this.#usingBulkDocs = [];
     this.#rawData.filter((data) => data.stat === '3').forEach((raw) => {
       const logId = `${date.week}${raw.statId}${raw.chgerId}`;
-      this.#logsForBulk.push(
+      this.#usingBulkDocs.push(
         updateUsingLogDoc(this.#statusManager.getTime(), logId),
       );
     });
-    console.log(`${this.#region + this.#currentPage} 사용중인 충전기 수 : ${this.#logsForBulk.length}`);
-    await StationLogs.bulkWrite(this.#logsForBulk).then((bulkWriteOpResult) => {
-      console.log(`[logger] [사용중인 충전소 로그 업데이트] ${this.#region + this.#currentPage} MongoDB BULK update OK : ${this.#logsForBulk.length}`.green);
+    console.log(`${this.#region + this.#currentPage} 사용중인 충전기 수 : ${this.#usingBulkDocs.length}`);
+    await StationLogs.bulkWrite(this.#usingBulkDocs).then((bulkWriteOpResult) => {
+      console.log(`[logger] [사용중인 충전소 로그 업데이트] ${this.#region + this.#currentPage} MongoDB BULK update OK : ${this.#usingBulkDocs.length}`.green);
     }).catch((err) => {
       console.log(`>> Logs [사용중인 충전소 로그 업데이트] ${this.#region + this.#currentPage} BULK update error`);
       console.log(JSON.stringify(err));
