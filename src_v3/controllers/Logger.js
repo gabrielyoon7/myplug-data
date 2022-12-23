6610cc2a40e67ba6baeaa7169416beb75c94ed9a
gabrielyoon7 <gabrielyoon7@gmail.com>
2022-12-23 16:32:01 +0900

    refactor: 일부 메소드 모듈화
    - Logger 클래스 함수 분리가 필요

--- src_v3/controllers/Logger.js
+++ src_v3/controllers/Logger.js
@@ -1,4 +1,5 @@
 import { StationLogs } from '../models/StationLogs.js';
+import { insertDefaultLog, updateUsingLog } from '../utils/common.js';
 
 export default class Logger {
   #statusManager = null;
@@ -10,33 +11,6 @@ export default class Logger {
   #rawData = null;
 
   #logsForBulk = [];
-
-  #defaultTimeTable = {
-    0: 0,
-    1: 0,
-    2: 0,
-    3: 0,
-    4: 0,
-    5: 0,
-    6: 0,
-    7: 0,
-    8: 0,
-    9: 0,
-    10: 0,
-    11: 0,
-    12: 0,
-    13: 0,
-    14: 0,
-    15: 0,
-    16: 0,
-    17: 0,
-    18: 0,
-    19: 0,
-    20: 0,
-    21: 0,
-    22: 0,
-    23: 0,
-  };
 
   constructor(statusManager, region, currentPage, rawData) {
     this.#statusManager = statusManager;
@@ -63,7 +37,9 @@ export default class Logger {
       // eslint-disable-next-line no-underscore-dangle
       const index = logs.findIndex((item) => (item._id === newId));
       if (index === -1) {
-        this.#logsForBulk.push(this.addDefaultLogJSON(newId, raw));
+        this.#logsForBulk.push(
+          insertDefaultLogDoc(this.#statusManager.getTime(), this.#region, newId, raw),
+        );
       }
     });
     console.log(`[logger] ${this.#region + this.#currentPage} next logs size : ${this.#logsForBulk.length}`);
@@ -77,7 +53,9 @@ export default class Logger {
     this.#logsForBulk = [];
     this.#rawData.filter((data) => data.stat === '3').forEach((raw) => {
       const logId = `${date.week}${raw.statId}${raw.chgerId}`;
-      this.#logsForBulk.push(this.addStat3LogJSON(logId));
+      this.#logsForBulk.push(
+        updateUsingLogDoc(this.#statusManager.getTime(), logId),
+      );
     });
     console.log(`${this.#region + this.#currentPage} 사용중인 충전기 수 : ${this.#logsForBulk.length}`);
     await StationLogs.bulkWrite(this.#logsForBulk).then((bulkWriteOpResult) => {
@@ -87,65 +65,5 @@ export default class Logger {
       console.log(JSON.stringify(err));
     });
     this.#statusManager.updateStatus(this.#region, this.#currentPage, 'logger', true);
-  }
-
-  /**
- * 사용중인 충전기 업데이트
- * @param {*} logId
- * @returns
- */
-  addStat3LogJSON(logId) {
-  // https://github.com/Automattic/mongoose/issues/9268 여기 참고해서 다시 도전해보기 --> 해결
-  // console.log(logId);
-    const { day } = this.#statusManager.getTime();
-    const { hour } = this.#statusManager.getTime();
-    const { week } = this.#statusManager.getTime();
-    const temp = `logs.${day}.${hour}`;
-    const doc = {
-      updateOne: {
-        filter: {
-          $and: [
-            { _id: { $eq: logId } },
-            { week: { $eq: week } },
-          ],
-        },
-        update: {
-          $set: {
-            [temp]: 1,
-          },
-        },
-      },
-    };
-    return doc;
-  }
-
-  /**
-* 처음 발견한 충전기에 대한 기본 로그 추가
-* @param {*} newId
-* @param {*} raw
-* @returns
-*/
-  addDefaultLogJSON(newId, raw) {
-    const upsertDoc = {
-      insertOne: {
-        document: {
-          _id: newId,
-          statId: raw.statId,
-          chgerId: raw.chgerId,
-          week: this.#statusManager.getTime().week,
-          region: this.#region,
-          logs: {
-            mon: this.#defaultTimeTable,
-            tue: this.#defaultTimeTable,
-            wed: this.#defaultTimeTable,
-            thu: this.#defaultTimeTable,
-            fri: this.#defaultTimeTable,
-            sat: this.#defaultTimeTable,
-            sun: this.#defaultTimeTable,
-          },
-        },
-      },
-    };
-    return upsertDoc;
   }
 }
--- src_v3/utils/common.js
+++ src_v3/utils/common.js
@@ -61,4 +61,95 @@ const chargerUpsertDoc = (date, raw) => {
   return upsertDoc;
 };
 
-export { stationUpsertDoc, chargerUpsertDoc };
+const defaultTimeTable = {
+  0: 0,
+  1: 0,
+  2: 0,
+  3: 0,
+  4: 0,
+  5: 0,
+  6: 0,
+  7: 0,
+  8: 0,
+  9: 0,
+  10: 0,
+  11: 0,
+  12: 0,
+  13: 0,
+  14: 0,
+  15: 0,
+  16: 0,
+  17: 0,
+  18: 0,
+  19: 0,
+  20: 0,
+  21: 0,
+  22: 0,
+  23: 0,
+};
+
+/**
+* 사용중인 충전기 업데이트
+* @param {*} logId
+* @returns
+*/
+const updateUsingLogDoc = (time, logId) => {
+  // https://github.com/Automattic/mongoose/issues/9268 여기 참고해서 다시 도전해보기 --> 해결
+  // console.log(logId);
+  const { day } = time;
+  const { hour } = time;
+  const { week } = time;
+  const temp = `logs.${day}.${hour}`;
+  const doc = {
+    updateOne: {
+      filter: {
+        $and: [
+          { _id: { $eq: logId } },
+          { week: { $eq: week } },
+        ],
+      },
+      update: {
+        $set: {
+          [temp]: 1,
+        },
+      },
+    },
+  };
+  return doc;
+};
+
+/**
+* 처음 발견한 충전기에 대한 기본 로그 추가
+* @param {*} time
+* @param {*} region
+* @param {*} newId
+* @param {*} raw
+* @returns
+*/
+const insertDefaultLogDoc = (time, region, newId, raw) => {
+  const upsertDoc = {
+    insertOne: {
+      document: {
+        _id: newId,
+        statId: raw.statId,
+        chgerId: raw.chgerId,
+        week: time.week,
+        region,
+        logs: {
+          mon: defaultTimeTable,
+          tue: defaultTimeTable,
+          wed: defaultTimeTable,
+          thu: defaultTimeTable,
+          fri: defaultTimeTable,
+          sat: defaultTimeTable,
+          sun: defaultTimeTable,
+        },
+      },
+    },
+  };
+  return upsertDoc;
+};
+
+export {
+  stationUpsertDoc, chargerUpsertDoc, updateUsingLogDoc, insertDefaultLogDoc,
+};
